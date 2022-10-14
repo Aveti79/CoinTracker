1ee5a09972299f7d8d7805ec665bea72978a4456
Lukasz Ptaszek <lukasz.ptaszek@interia.pl>
2022-10-14 17:16:35 +0200

    Added working autocomplete for Currency in addTransactionPanel

--- src/main/java/com/aveti/CoinTracker/controller/CurrencyController.java
+++ src/main/java/com/aveti/CoinTracker/controller/CurrencyController.java
@@ -1,6 +1,6 @@
 package com.aveti.CoinTracker.controller;
 
-import com.aveti.CoinTracker.logic.CoinService;
+import com.aveti.CoinTracker.logic.CurrencyService;
 import com.aveti.CoinTracker.model.CurrencyDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,14 +11,14 @@ import java.util.List;
 @RestController
 public class CurrencyController {
 
-    private CoinService coinService;
+    private CurrencyService currencyService;
 
-    public CurrencyController(final CoinService coinService) {
-        this.coinService = coinService;
+    public CurrencyController(final CurrencyService currencyService) {
+        this.currencyService = currencyService;
     }
 
     @GetMapping("/coinNameAutocomplete")
     public List<CurrencyDTO> coinNameAutocomplete(@RequestParam(value = "term", required = false, defaultValue = "") String term) {
-        return coinService.coinAutocomplete(term);
+        return currencyService.currencyAutocomplete(term);
     }
 }
