3a5808e3d83b741e29e2677cfd67226f5c5c4e03
Lukasz Ptaszek <luk.ptaszek@gmail.com>
2023-02-06 20:44:13 +0100

    Added Icons to autocomplete list in addTransactionPanel.html Changed Query in autocomplete to return better data set. Fixed some issues with autocomplete and moved script to external file

--- src/main/java/com/aveti/CoinTracker/logic/CurrencyService.java
+++ src/main/java/com/aveti/CoinTracker/logic/CurrencyService.java
@@ -1,53 +1,42 @@
 package com.aveti.CoinTracker.logic;
 
-import com.aveti.CoinTracker.model.Currency;
-import com.aveti.CoinTracker.model.CurrencyDTO;
 import com.aveti.CoinTracker.model.repository.CurrencyRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 @Service
 public class CurrencyService {
 
     private final CurrencyRepository currencyRepository;
-    @SuppressWarnings("FieldMayBeFinal")
-    private List<CurrencyDTO> suggestionsList = new ArrayList<>();
+    private String[][] autocompleteList;
     private String lastKeyword;
 
     CurrencyService(final CurrencyRepository currencyRepository) {
         this.currencyRepository = currencyRepository;
     }
 
-    public List<CurrencyDTO> currencyAutocomplete(String keyword) {
-        List<CurrencyDTO> currencyDTOList = new ArrayList<>();
+    public String[][] currencyAutocomplete(String keyword) {
+        final String finalKeyword = keyword.toLowerCase();
+        String[][] resultList;
 
         if (lastKeyword!=null) {
-            if (keyword.startsWith(lastKeyword.substring(0,2)) && suggestionsList!=null) {
-                for (CurrencyDTO currency : suggestionsList) {
-                    if (currency.getLabel().toLowerCase().contains(keyword.toLowerCase()) || currency.getValue().contains(keyword)) {
-                        currencyDTOList.add(currency);
-                    }
-                }
-                lastKeyword=keyword;
-                return currencyDTOList;
-            } else if (suggestionsList!=null){
-                suggestionsList.clear();
+            if (keyword.startsWith(lastKeyword.substring(0,1)) && autocompleteList!=null) {
+                resultList = Arrays.stream(autocompleteList)
+                        .filter((currency) ->
+                            currency[0].contains(finalKeyword) ||
+                            currency[1].toLowerCase().contains(finalKeyword) ||
+                            currency[2].contains(finalKeyword))
+                        .limit(20)
+                        .toArray(String[][]::new);
+                lastKeyword=finalKeyword;
+                return resultList;
+            } else if (autocompleteList!=null){
+                autocompleteList = null;
             }
         }
-        for (Currency currency : currencyRepository.findCurrencyByKeyword(keyword.toLowerCase())) {
-            getResultChangedToDTO(currencyDTOList,currency);
-        }
-        suggestionsList.addAll(currencyDTOList);
+        autocompleteList = currencyRepository.findCurrencyByKeyword(finalKeyword.substring(0,1));
         lastKeyword=keyword;
-        return currencyDTOList;
-    }
-
-    private void getResultChangedToDTO(final List<CurrencyDTO> currencyDTOList, final Currency coin) {
-        CurrencyDTO dto = new CurrencyDTO();
-        dto.setLabel(coin.getSymbol().toUpperCase() + " " + coin.getName() + " (" + coin.getClass().getSimpleName() + ")");
-        dto.setValue(coin.getId());
-        currencyDTOList.add(dto);
+        return Arrays.stream(autocompleteList).limit(20).toArray(String[][]::new);
     }
 }
