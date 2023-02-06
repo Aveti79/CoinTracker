fae81651e91f42a2714b66214f171509da6e1197
Lukasz Ptaszek <luk.ptaszek@gmail.com>
2023-02-06 20:27:36 +0100

    Added CoinDetails Service and Repository to fetch information about icons for specified currency.

--- src/main/java/com/aveti/CoinTracker/model/repository/CoinDetailsRepository.java
+++ src/main/java/com/aveti/CoinTracker/model/repository/CoinDetailsRepository.java
@@ -4,6 +4,11 @@ import com.aveti.CoinTracker.model.CoinDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CoinDetailsRepository extends JpaRepository<CoinDetails, Integer> {
+
+    Optional<CoinDetails> findById(String coinId);
+
 }
